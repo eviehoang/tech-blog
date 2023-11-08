83544da6691d285f7555a5c8f4aa20dbbcd8fcb5
Evie Hoang <evie.h0325@gmail.com>
2023-11-07 17:19:09 -0800

    Updated comments.js models

--- models/Comments.js
+++ models/Comments.js
@@ -1,4 +1,46 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-class Comment extends Model {}
+class Comment extends Model { }
+
+Comment.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    comment_body: {
+        type: DataTypes.STRING,
+    },
+    date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+    },
+    blog_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'blog',
+            key: 'id',
+        },
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id',
+        },
+    },
+},
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+module.exports = Comment;
+
 
